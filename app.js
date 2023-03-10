a1d7c5e165643ac77d13c913c2f4bb941a999c0b
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-03-10 16:26:03 +0700

    Third Party Middle Ware & Install package Morgan

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express') // require express
 const app = express()
-
 const port = 5000;
-
 const expressLayouts = require('express-ejs-layouts');
+
+const morgan = require('morgan');
 
 // panggil ejs templating 
 app.set('view engine', 'ejs');
-app.use(expressLayouts);
 
+// Third Party Middle Ware
+app.use(expressLayouts);
+app.use(morgan('dev'));
 // Build in Middleware
 app.use(express.static('public'));
 
